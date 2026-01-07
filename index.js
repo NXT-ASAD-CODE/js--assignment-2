41c76969844af467cf833f413f6b9d53f299c96f
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-01-07 09:34:04 +0500

    relult added

--- index.js
+++ index.js
@@ -25,4 +25,35 @@ function showPassword() {
     }
  //   Odd even or prime checker
  var num = document.getElementById("number").value
- var result = document.getElementById
+ var result = document.getElementById("result")
+ function checkNumber() {
+    var num = parseInt(document.getElementById("number-checker").value);
+    var result = document.querySelector(".result");
+    if (isNaN(num)) {
+        result.innerText = "Please enter a valid number.";
+        return;
+    }
+    if (num <= 1) {
+        result.innerText = num + " is neither odd, even, nor prime.";
+        return;
+    }
+    if (num % 2 === 0) {
+        result.innerText = num + " is an even number.";
+    } else {
+        result.innerText = num + " is an odd number.";
+    }
+    // Check for prime
+    var isPrime = true;
+    for (var i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime) {
+        result.innerText += " It is also a prime number.";
+    } else {
+
+        result.innerText += " It is not a prime number.";
+    }
+    }
